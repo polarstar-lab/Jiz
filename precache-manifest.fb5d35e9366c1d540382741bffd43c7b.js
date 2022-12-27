339ddea470f3168abe3b46b923197d3e72f6d731
polarstar-lab <polarstar-lab@users.noreply.github.com>
2022-12-27 09:10:58 +0000

    deploy: c09e38aa242ff4b707d935cb4a7969fd184f120d

---
+++ precache-manifest.fb5d35e9366c1d540382741bffd43c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3080eb49778e49b9f413",
+    "url": "/css/app.a8200ca7.css"
+  },
+  {
+    "revision": "fa258f9e06974a67ba96311d93a7c51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3080eb49778e49b9f413",
+    "url": "/js/app.05b79640.js"
+  },
+  {
+    "revision": "f6e5e001d5ca7fc9aedd",
+    "url": "/js/chunk-vendors.e18fdaba.js"
+  },
+  {
+    "revision": "02adcb277ba6fd882d3307d3966c1370",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
